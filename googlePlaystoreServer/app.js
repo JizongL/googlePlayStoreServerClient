cf4c270cbcf2881691ecf46a43749539d06bd2fa
JizongL <2015rpro@gmail.com>
2019-04-02 22:41:56 -0400

    nothing has done, just recommit

--- googlePlaystoreServer/app.js
+++ googlePlaystoreServer/app.js
@@ -7,24 +7,54 @@ app.use(morgan('common'))
 app.use(cors())
 
 app.get('/apps',(req,res)=>{
-  const{genre="",sort}=req.query
+  let {genre='',sort}=req.query
   let genreArray= ['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'].map(genre=> genre.toLowerCase())
-  
-  if(genre){
+  let sortArray = ['rating','app']
 
-    if(!genreArray.includes(genre)){
-      res.status(400).send('Please Provide a valid genre')
+
+  if(!genreArray.includes(genre)){
+    res.send('provide a valid genre')
+    return 
+  }
+
+
+    if(!genre){
+      res.send(playstore)
+      return
     }
 
     let searchResult = playstore.filter(app=>{
-      console.log(app.Genres.toLowerCase().includes(genre),genre)
+      console.log(app.Genres.toLowerCase().includes(genre))
+  
+        return app.Genres.toLowerCase().includes(genre)
+        
+      })
 
-      return app.Genres.toLowerCase().includes(genre)
+
+      if(sort){
+
+        if(!sortArray.includes(sort.toLowerCase())){
+          res.status(400).send('Provide valid sort term')
+          return 
+        }
+        sort=sort[0].toUpperCase()+sort.slice(1)
+        searchResult.sort((a,b)=>{
+          return a[sort]>b[sort]?1:a[sort]<b[sort]?-1:0
+        })
+      }
+
+
       
-    })
-    console.log(searchResult)
-  }
-  res.send('dddddd')
+      let newResult = searchResult.map(element=>element)
+      
+    
+
+  
+  res.json(newResult)
+
+    
+
+
 
 })
 
